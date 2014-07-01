c599e218e6093821d0085ecd12652da9b2320042
Leah Steinberg <leah.steinberg@venmo.com>
2014-07-01 12:50:32 -0400

    test modifications

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ app.get('/start', function(req, res){
 	 res.render('signin', { title: 'Express' });
 	 
 });
+app.get('/webhook_url', function(req, res){
+	console.log("hitting initialize webhook url!!");
+    console.log(req);
+    var venmo_response = req.query['venmo_challenge'];
 
+    console.log("venmo_response is.. ", venmo_response);
+	
+    res.send(venmo_response, "stuff");
+
+});
 app.get('/login', function(req, res){
     var redirect_string = "https://api.venmo.com/v1/oauth/authorize?client_id="+ CLIENT_ID+"&scope=make_payments+access_balance+access_friends+access_email+access_feed%20access_profile&response_type=code";	
     res.json(redirect_string);
@@ -29,24 +38,23 @@ app.get('/venmocallback', function(req, res){
 	console.log("code is:!     ", req.query['code']);
 	AUTH_CODE = req.query['code'];
 
-    "express-session": "^1.0`.3",
-    ns = {host: 'api.venmo.com',
-						path: '/v1/me?access_token='+req.session.access_token,
-    					method: 'GET'}
+//    ns = {host: 'api.venmo.com',
+///	path: '/v1/me?access_token='+req.session.access_token,
+ //   	method: 'GET'}
 
-	var req = https.get(options, function(http_res){
+//	var req = https.get(options, function(http_res){
 	    		//console.log('STATUS:', http_res.statusCode);
-	http_res.on('data', function (chunk) {
-	res.json(JSON.parse(chunk));
-	console.log("data from venmo user, ", JSON.parse(chunk));
-	});
-	})   		
-	req.on('error', function(e) {
-  		    		console.error(e);
-	});
-);
+//		http_res.on('data', function (chunk) {
+//			res.json(JSON.parse(chunk));
+//			console.log("data from venmo user, ", JSON.parse(chunk));
+//		});
+//	})   		
+//	req.on('error', function(e) {
+  //  		console.error(e);
+//	});
+});
 
-var server = app.listen(4040, function(){
+var server = app.listen(4040, "0.0.0.0", function(){
 	console.log('listening on port %d', server.address().port);
 });
 
